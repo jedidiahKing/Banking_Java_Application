fddefd4b19a6d4bf6532593059c51c1910295750
jedy <you@example.com>
2021-03-18 11:02:37 +0000

    Customer Test added

---
+++ src/test/java/com/jedyBank/classes/CustomerTest.java
@@ -0,0 +1,30 @@
+package com.jedyBank.classes;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getTransaction() {
+    }
+
+    @Test
+    public void addTransaction() {
+    }
+}
