4cd4108aa9094f95c1f507d1f33ea7cb8dd5731a
jedy <you@example.com>
2021-03-18 11:03:57 +0000

    Branch test added

---
+++ src/test/java/com/jedyBank/classes/BranchTest.java
@@ -0,0 +1,34 @@
+package com.jedyBank.classes;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BranchTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getCustomers() {
+    }
+
+    @Test
+    public void newCustomer() {
+    }
+
+    @Test
+    public void addCustomerTransaction() {
+    }
+}
