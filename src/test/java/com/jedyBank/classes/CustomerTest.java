49cede8b0836a5b307bf2a35523db2eaca0e4e13
jedy <you@example.com>
2021-03-18 12:44:50 +0000

    customer Test done

--- src/test/java/com/jedyBank/classes/CustomerTest.java
+++ src/test/java/com/jedyBank/classes/CustomerTest.java
@@ -7,24 +7,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CustomerTest {
+    Customer customer;
 
     @Before
     public void setUp() throws Exception {
+        customer = new Customer("Jedy", 2500.00 );
     }
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
     public void getName() {
+        //this method get the name of the customer object and checks to see if it is "Jedy"
+        assertEquals("Jedy", customer.getName());
+        //this method get the name of the customer object and checks to see if it is "Jefferson" which is not
+        assertNotEquals("Jefferson", customer.getName());
     }
 
     @Test
     public void getTransaction() {
+        //this method get the amount in the transaction
+        assertEquals(2500.00, customer.getTransaction().get(0), 0.0);
+        // this method checks if the amount in the transaction matches
+        assertNotEquals(250.00, customer.getTransaction().get(0), 0.0);
     }
 
     @Test
     public void addTransaction() {
+        // adding new transaction
+        customer.addTransaction(200.00);
+        //checking the transactions added
+        assertEquals(2, customer.getTransaction().size());
     }
 }
